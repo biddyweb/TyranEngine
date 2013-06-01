1b11e7fbc646206f2e52fe63ca43a711e9e86cb5
Piot <piot@hotmail.com>
2013-06-01 17:30:14 +0200

    Added -o output path

--- src/buildscript/build_ios.sh
+++ src/buildscript/build_ios.sh
@@ -1,1 +1,2 @@
-../../Project-Generator/source/generate.py -i tyran.xml -p ios -g xcode -n sinkblobs -r ../resource/ $@
+../../Project-Generator/source/generate.py -i tyran.xml -p ios -g xcode -n sinkblobs -r ../resource/ -o ../project/ $@
+

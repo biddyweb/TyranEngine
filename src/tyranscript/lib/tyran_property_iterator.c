c05e84a15709e0e83da04c742127663096897831
Piot <piot@hotmail.com>
2013-07-09 01:34:40 +0200

    removed loggin on property iterator

--- src/tyranscript/lib/tyran_property_iterator.c
+++ src/tyranscript/lib/tyran_property_iterator.c
@@ -3,7 +3,6 @@
 
 void tyran_property_iterator_init(tyran_property_iterator* self, tyran_object* object)
 {
-	TYRAN_LOG("iterator %p init", self);
 	self->property_index = 0;
 	self->object = object;
 	self->visited_symbols_count = 0;
@@ -12,7 +11,6 @@ void tyran_property_iterator_init(tyran_property_iterator* self, tyran_object* o
 
 void tyran_property_iterator_free(tyran_property_iterator* self)
 {
-	TYRAN_LOG("iterator %p free", self);
 }
 
 static tyran_boolean have_visited_symbol(tyran_property_iterator* self, tyran_symbol symbol)
@@ -36,25 +34,20 @@ static void add_symbol(tyran_property_iterator* self, tyran_symbol symbol)
 tyran_boolean tyran_property_iterator_next(tyran_property_iterator* self, tyran_symbol* symbol, tyran_value** value)
 {
 	if (self->property_index >= self->object->property_count) {
-		TYRAN_LOG("No more properties on this object");
 		tyran_object* parent = self->object->prototype;
 		TYRAN_ASSERT(parent != self->object, "Prototype can not be self!");
 		if (!parent) {
-			TYRAN_LOG("property iterator end");
 			return TYRAN_FALSE;
 		} else {
 			self->property_index = 0;
 			self->object = parent;
-			TYRAN_LOG("going to parent");
 			return tyran_property_iterator_next(self, symbol, value);
 		}
 	} else {
 		tyran_object_property* property = &self->object->properties[self->property_index++];
 		if (have_visited_symbol(self, property->symbol)) {
-			TYRAN_LOG("we already been to this one, skip to next");
 			return tyran_property_iterator_next(self, symbol, value);
 		} else {
-			TYRAN_LOG("next property");
 			*symbol = property->symbol;
 			*value = &property->value;
 			add_symbol(self, property->symbol);

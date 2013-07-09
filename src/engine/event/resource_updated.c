86751d9792766da90a046dcb008307a3b4253c9a
Piot <piot@hotmail.com>
2013-07-09 17:52:04 +0200

    Added send helper function for resource updated event

--- src/engine/event/resource_updated.c
+++ src/engine/event/resource_updated.c
@@ -1,3 +1,20 @@
 #include "resource_updated.h"
+#include "../../core/src/base/event/nimbus_event_stream.h"
+#include <tyranscript/tyran_log.h>
 
 const u8t NIMBUS_EVENT_RESOURCE_UPDATED = 2;
+
+void nimbus_resource_updated_send(nimbus_event_write_stream* out_event_stream, nimbus_resource_id resource_id, nimbus_resource_type_id resource_type_id, void* data, int payload_size)
+{
+	nimbus_resource_updated updated;
+	updated.resource_id = resource_id;
+	updated.resource_type_id = resource_type_id;
+	updated.payload_size = payload_size;
+
+	TYRAN_LOG("Sending resource updated. %d of type %d", resource_id, resource_type_id);
+
+	nimbus_event_stream_write_event_header(out_event_stream, NIMBUS_EVENT_RESOURCE_UPDATED);
+	nimbus_event_stream_write_type(out_event_stream, updated);
+	nimbus_event_stream_write_octets(out_event_stream, data, payload_size);
+	nimbus_event_stream_write_event_end(out_event_stream);
+}
--- src/engine/event/resource_updated.h
+++ src/engine/event/resource_updated.h
@@ -4,6 +4,8 @@
 #include "../resource/resource_id.h"
 #include "../resource/resource_type_id.h"
 
+struct nimbus_event_write_stream;
+
 extern const u8t NIMBUS_EVENT_RESOURCE_UPDATED;
 
 typedef struct nimbus_resource_updated {
@@ -12,5 +14,7 @@ typedef struct nimbus_resource_updated {
 	int payload_size;
 } nimbus_resource_updated;
 
+
+void nimbus_resource_updated_send(struct nimbus_event_write_stream* out_event_stream, nimbus_resource_id resource_id, nimbus_resource_type_id resource_type_id, void* data, int payload_size);
 
 #endif

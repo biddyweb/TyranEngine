2fa7b6b7b1f0b4d4cd06caee249f5a3792b1332f
Piot <piot@hotmail.com>
2013-07-09 17:51:14 +0200

    Added resource load state event

---
+++ src/engine/event/resource_load_state.h
@@ -0,0 +1,17 @@
+#ifndef nimbus_resource_load_state_h
+#define nimbus_resource_load_state_h
+
+#include "../resource/resource_id.h"
+
+extern const u8t NIMBUS_EVENT_RESOURCE_LOAD_STATE;
+
+struct nimbus_event_write_stream;
+
+typedef struct nimbus_resource_load_state {
+	nimbus_resource_id resource_id;
+} nimbus_resource_load_state;
+
+void nimbus_resource_load_state_send(struct nimbus_event_write_stream* stream, nimbus_resource_id id);
+
+
+#endif

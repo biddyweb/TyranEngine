de79afc7bb05767c16c7729da4b533087c7d6e73
Piot <piot@hotmail.com>
2013-08-13 11:13:07 +0200

    Fixed memory leak in strings

--- src/tyranscript/include/tyranscript/tyran_string.h
+++ src/tyranscript/include/tyranscript/tyran_string.h
@@ -12,13 +12,12 @@ typedef struct tyran_string {
 	tyran_string_char* buf;
 } tyran_string;
 
-void tyran_string_free(const tyran_string* str);
 void tyran_string_init(tyran_string* self, tyran_memory* memory, const tyran_string_char* chars, int char_count);
 const tyran_string* tyran_string_from_c_str(tyran_memory_pool* string_pool, tyran_memory* memory, const char* str);
 const tyran_string* tyran_string_from_characters(tyran_memory_pool* string_pool, tyran_memory* memory, const tyran_string_char* str, int len);
 const tyran_string* tyran_string_strcat(tyran_memory_pool* string_pool, tyran_memory* memory, const tyran_string* str1, const tyran_string* str2);
 const tyran_string* tyran_string_strdup(tyran_memory_pool* string_pool, tyran_memory* memory, const tyran_string* str);
-void tyran_string_free(const tyran_string* d);
+void tyran_string_free(tyran_string* d);
 
 const tyran_string* tyran_string_strcpy(tyran_memory_pool* string_pool, tyran_memory* memory, const tyran_string* from);
 const tyran_string* tyran_string_substr(tyran_memory_pool* string_pool, tyran_memory* memory, const tyran_string* str, int start, int len);

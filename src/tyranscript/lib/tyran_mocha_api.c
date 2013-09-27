cccfa89bcc4fb14121e5dcffd21cda550050cbbb
Piot <piot@hotmail.com>
2013-09-27 23:31:47 +0200

    Removed dead code

--- src/tyranscript/lib/tyran_mocha_api.c
+++ src/tyranscript/lib/tyran_mocha_api.c
@@ -57,7 +57,6 @@ void tyran_mocha_api_new(tyran_mocha_api* api, int hunk_size)
 void tyran_mocha_api_eval(tyran_mocha_api* api, tyran_value* context, tyran_value* return_value, const char* buf)
 {
 	tyran_mocha_lexer* mocha_lexer = tyran_mocha_lexer_lex(api->mocha_lexer_pool, api->mocha_token_pool, api->lexer_pool, api->memory, buf);
-	// tyran_mocha_lexer_debug(mocha_lexer);
 
 	const int default_size = 100;
 
@@ -67,17 +66,12 @@ void tyran_mocha_api_eval(tyran_mocha_api* api, tyran_value* context, tyran_valu
 	tyran_memory_pool* parser_node_parameter_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_parser_node_parameter, default_size);
 	tyran_memory_pool* mocha_token_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_mocha_token, default_size);
 
-
 	struct tyran_parser_node* parser_tree =  tyran_mocha_parser_parse(api->memory, mocha_lexer, mocha_parser_pool, enclosure_stack_pool, enclosure_info_pool, parser_node_parameter_pool, mocha_token_pool);
 
-
-	// tyran_parser_node_print("result", parser_tree, 0);
 	tyran_memory_pool* code_state_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_code_state, default_size);
 	tyran_memory_pool* opcodes_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_opcodes, default_size);
 	tyran_memory_pool* constants_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_constants, default_size);
 	tyran_memory_pool* constant_values_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_value, default_size);
-	// tyran_memory_pool* label_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_label, default_size);
-	// tyran_memory_pool* label_reference_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_label_reference, default_size);
 
 	tyran_memory_pool* function_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_function, default_size);
 	tyran_memory_pool* string_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_string, default_size * 3);
@@ -87,12 +81,10 @@ void tyran_mocha_api_eval(tyran_mocha_api* api, tyran_value* context, tyran_valu
 
 	tyran_memory_pool* generator_pool = TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_generator, default_size);
 	tyran_generator_new(api->memory, generator_pool, parser_tree, code);
-
 	// tyran_print_opcodes(code->opcodes, 0, code->constants);
 
 	tyran_runtime_push_call(api->default_runtime, code->opcodes, code->constants, context);
 	tyran_runtime_execute(api->default_runtime, return_value, 0);
-	// struct tyran_memory_pool* object_iterator_pool = 0; // TYRAN_MEMORY_POOL_CONSTRUCT(api->memory, tyran_object_iterator, 10);
 	// tyran_print_value("return", return_value, 1, code->constants->symbol_table, object_iterator_pool, api->string_pool, api->memory);
 }
 

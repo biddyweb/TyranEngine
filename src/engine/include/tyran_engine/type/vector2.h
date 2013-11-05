44c3bb197b329a50abb3114c84fc4c866613a510
Piot <piot@hotmail.com>
2013-11-05 19:53:47 +0100

    Added mul_scalar, add and length for vector2

--- src/engine/include/tyran_engine/type/vector2.h
+++ src/engine/include/tyran_engine/type/vector2.h
@@ -9,5 +9,8 @@ typedef struct nimbus_vector2 {
 } nimbus_vector2;
 
 nimbus_vector2 nimbus_vector2_make(tyran_number a, tyran_number b);
+nimbus_vector2 nimbus_vector2_mul_scalar(nimbus_vector2 p, tyran_number scalar);
+nimbus_vector2 nimbus_vector2_add(nimbus_vector2 a, nimbus_vector2 b);
+tyran_number nimbus_vector2_length(nimbus_vector2 a);
 
 #endif
--- src/engine/type/vector2.c
+++ src/engine/type/vector2.c
@@ -1,4 +1,5 @@
 #include <tyran_engine/type/vector2.h>
+#include <tyran_engine/math/nimbus_math.h>
 
 nimbus_vector2 nimbus_vector2_make(tyran_number a, tyran_number b)
 {
@@ -8,4 +9,19 @@ nimbus_vector2 nimbus_vector2_make(tyran_number a, tyran_number b)
 	point.y = b;
 
 	return point;
+}
+
+nimbus_vector2 nimbus_vector2_mul_scalar(nimbus_vector2 p, tyran_number scalar)
+{
+	return nimbus_vector2_make(p.x * scalar, p.y * scalar);
+}
+
+nimbus_vector2 nimbus_vector2_add(nimbus_vector2 a, nimbus_vector2 b)
+{
+	return nimbus_vector2_make(a.x + b.x, a.y + b.y);
+}
+
+tyran_number nimbus_vector2_length(nimbus_vector2 a)
+{
+	return nimbus_math_sqrt(a.x * a.x + a.y * a.y);
 }

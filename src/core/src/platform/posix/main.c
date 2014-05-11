8b60f14504761ad157b98915d7659bff276d01b5
Peter Björklund <piot@hotmail.com>
2014-05-11 22:13:27 +0200

    SIGINT handler

--- src/core/src/platform/posix/main.c
+++ src/core/src/platform/posix/main.c
@@ -4,13 +4,25 @@
 #include <tyranscript/tyran_log.h>
 #include "log.h"
 
+#include <signal.h>
+
+int g_terminate = 0;
+
+static void signal_callback_handler(int signum)
+{
+	if (signum == SIGTERM || signum == SIGINT) {
+		g_terminate = 1;
+	}
+}
+
 int main()
 {
+	signal(SIGINT, signal_callback_handler);
 	g_log.log = nimbus_log_posix;
 
 	nimbus_boot* boot = nimbus_boot_new();
 
-	while (1) {
+	while (!g_terminate) {
 		printf("main::update\n");
 		if (nimbus_boot_ready_for_next_frame(boot)) {
 			int err = nimbus_boot_update(boot);
@@ -20,6 +32,11 @@ int main()
 		}
 		nimbus_thread_sleep(0.5f);
 	}
+
+	TYRAN_LOG("main::destroy");
 	nimbus_boot_destroy(boot);
+
+	TYRAN_LOG("main::exit");
+
 	return 0;
 }
